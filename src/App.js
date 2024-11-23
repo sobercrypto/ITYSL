f2f13454241080cd100900aed53edb683b7e8c2a
Alex <sobercrypto@proton.me>
2024-11-22 22:31:04 -0500

    fixed merge conflict

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import MainPage from './pages/MainPage';
@@ -19,15 +18,6 @@ function App() {
         <Route path="/gallery" element={<GalleryPage />} />
       </Routes>
     </Router>
-=======
-import Door from './components/Door';
-
-function App() {
-  return (
-    <div className="App">
-      <Door />
-    </div>
->>>>>>> ddccfe72a0f7b78b414b4f349937d3ec162dbbaf
   );
 }
 
