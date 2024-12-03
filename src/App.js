948b054b1eedc5ea4da93a33393e5b63b834da1f
SQ <sobercrypto@proton.me>
2024-12-03 11:59:02 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import MainPage from './pages/MainPage';
-import QuotesPage from './pages/QuotesPage';
-// import CharactersPage from './pages/CharactersPage';
-// import EpisodesPage from './pages/EpisodesPage';
-// import GalleryPage from './pages/GalleryPage';
+import React from 'react';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/quotes" element={<QuotesPage />} />
-        <Route path="/characters" element={<CharactersPage />} />
-        <Route path="/episodes" element={<EpisodesPage />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-      </Routes>
-    </Router>
+    <div className="bg-black min-h-screen">
+      <LandingPage />
+    </div>
   );
 }
 
