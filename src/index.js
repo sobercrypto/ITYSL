bc3a623bec3c856e952252037ede9f95734ee52c
st4tic <sobercrypto@proton.me>
2024-12-03 13:05:48 -0500

    initial commit with basic youtube video landing page

---
+++ src/index.js
@@ -0,0 +1,15 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
